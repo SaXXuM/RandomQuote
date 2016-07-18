64d67ce0c9bb8451f29c5a987503b708b93606f9
SaXXuM <saxxum@gmail.com>
2016-07-18 23:48:48 +0600

    Added design and animation.

--- js/main.js
+++ js/main.js
@@ -1,37 +1,49 @@
 function documentReady() {
+
+    var backgroundColors = ['#ef5350', '#ec407a', '#ab47bc', '#7e57c2', '#5c6bc0', '#42a5f5', '#29b6f6', '#26c6da',
+        '#26a69a', '#66bb6a', '#9ccc65', '#d4e157', '#ffee58', '#ffca28', '#ffa726', '#ff7043', '#8d6e63', '#bdbdbd'];
+
     function getRandomQuote() {
+        console.log('Retrieve quote');
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', true);
+        xhr.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&r='+Math.random(), true);
         xhr.responseType = 'json';
 
         xhr.onreadystatechange = function() {
             if (this.readyState != 4) return;
 
-            // по окончании запроса доступны:
-            // status, statusText
-            // responseText, responseXML (при content-type: text/xml)
-
             if (this.status != 200) {
-                // обработать ошибку
-                console.log( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
+                console.log( 'Ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
                 return;
             }
+            console.log('Recided quote ' + this.response[0].content, this.response[0].title);
 
             showQuote(this.response[0].content, this.response[0].title);
-            // получить результат из this.responseText или this.responseXML
         };
 
         xhr.send();
-
     }
 
     function showQuote(quote, author) {
-        document.querySelector('#quote_widget .quote').innerHTML = quote;
-        document.querySelector('#quote_widget .author').innerHTML = author;
+        var newColor = backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+        document.querySelector('#quote_widget blockquote').innerHTML = '<i class="fa fa-quote-left" aria-hidden="true"></i> ' + stripTags(quote);
+        document.querySelector('#quote_widget .author').innerHTML = stripTags(author);
+        document.body.style.backgroundColor = newColor;
+        document.querySelector('#quote_widget button').style.backgroundColor = newColor;
     }
+
+    document.querySelector('#quote_widget button').addEventListener('click', getRandomQuote);
 
     getRandomQuote();
 }
+
+function stripTags(html)
+{
+    var tmp = document.createElement("DIV");
+    tmp.innerHTML = html;
+    return tmp.textContent || tmp.innerText || "";
+}
+
 
 document.addEventListener("DOMContentLoaded", documentReady);
 
